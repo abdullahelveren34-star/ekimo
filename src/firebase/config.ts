8cda666df33d88b25e595c5d67485c750bd4b15c
abdullah elveren <abdullah.elveren34@gmail.com>
2026-01-07 18:58:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4024231416-ba40f",
+  "appId": "1:593589377476:web:263f2c7cb7ed479ed9ec91",
+  "apiKey": "AIzaSyCAmEw42Ttn9hG5HMNZNvAE7nd11fySiWw",
+  "authDomain": "studio-4024231416-ba40f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "593589377476"
+};
