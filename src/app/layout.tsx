0ea5ea94e436d10ada2ac230e5dfde4ea8c8b307
abdullah elveren <abdullah.elveren34@gmail.com>
2026-01-07 19:04:32 +0000

    Try fixing this error: `Runtime Error: useFirebase must be used within a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { SidebarProvider, Sidebar, SidebarInset } from '@/components/ui/sidebar'
 import { SidebarNav } from '@/components/layout/sidebar-nav';
 import { Toaster } from '@/components/ui/toaster';
 import { UserProfile } from '@/components/layout/user-profile';
+import { FirebaseClientProvider } from '@/firebase';
 
 export const metadata: Metadata = {
   title: 'E-Kimo',
@@ -23,21 +24,23 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <SidebarProvider>
-          <Sidebar>
-            <SidebarNav />
-          </Sidebar>
-          <SidebarInset>
-            <div className="flex flex-col min-h-screen">
-              <header className="flex items-center justify-end p-4 border-b">
-                  <UserProfile />
-              </header>
-              <main className="flex-grow p-4 sm:p-6 lg:p-8">
-                {children}
-              </main>
-            </div>
-          </SidebarInset>
-        </SidebarProvider>
+        <FirebaseClientProvider>
+          <SidebarProvider>
+            <Sidebar>
+              <SidebarNav />
+            </Sidebar>
+            <SidebarInset>
+              <div className="flex flex-col min-h-screen">
+                <header className="flex items-center justify-end p-4 border-b">
+                    <UserProfile />
+                </header>
+                <main className="flex-grow p-4 sm:p-6 lg:p-8">
+                  {children}
+                </main>
+              </div>
+            </SidebarInset>
+          </SidebarProvider>
+        </FirebaseClientProvider>
         <Toaster />
       </body>
     </html>
